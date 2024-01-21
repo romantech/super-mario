59f67a97ace6ce848d17118d0a5bb96d907d2350
ColorFilter <johan@romantech.net>
2024-01-21 21:16:26 +0900

    :triangular_flag_on_post: preload mario image

--- src/mario.js
+++ src/mario.js
@@ -4,12 +4,19 @@ class Mario {
   static jumpHeight = 18;
   static gravity = 0.4; // 중력 가속도
 
+  stopImage = new Image();
+  runImage = new Image();
+  element = new Image();
+
   constructor({ defaultBottom, className = 'mario' }) {
     this.defaultBottom = defaultBottom;
     this.isJumping = false;
 
-    this.element = document.createElement('img');
-    this.element.src = '/assets/mario-stop.png';
+    // image.src 속성에 값을 할당하면 백그라운드에서 이미지 로드 시작
+    this.stopImage.src = './assets/mario-stop.png';
+    this.runImage.src = './assets/mario-run.gif';
+
+    this.element.src = this.stopImage.src; // 이미 로드한 이미지를 캐시에서 가져옴
     this.element.classList.add(className);
     this.element.style.bottom = defaultBottom + 'px';
 
@@ -17,12 +24,11 @@ class Mario {
   }
 
   run() {
-    // Github Page 에서 루트 폴더는 {username}.github.io 이므로 주소에 레포지토리 이름 추가
-    this.element.src = 'super-mario/assets/mario-run.gif';
+    this.element.src = this.runImage.src;
   }
 
   stop() {
-    this.element.src = 'super-mario/assets/mario-stop.png';
+    this.element.src = this.stopImage.src;
   }
 
   jump() {
